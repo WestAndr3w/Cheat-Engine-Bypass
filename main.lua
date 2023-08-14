e3fb2df1635d9b341a56bf7015d125677d5118e8
WestAndr3w <142079093+WestAndr3w@users.noreply.github.com>
2023-08-14 10:50:16 +0700

    14/8/2023

---
+++ main.lua
@@ -0,0 +1,6 @@
+--This lua script gets loaded when CTRminhh loads
+--You can use this to define some often used functions and libraries you'd like to use
+
+require("defines")
+
+--for documentation read celua.txt
